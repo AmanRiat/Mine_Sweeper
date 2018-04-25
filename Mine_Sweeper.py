41d7c5b79d63d1adc891fe5e5fd8402f62c2227b
david19tran <david19tran@gmail.com>
2018-04-24 23:32:48 -0400

    Add files via upload

--- Mine_Sweeper.py
+++ Mine_Sweeper.py
@@ -1,219 +1,283 @@
-from tkinter import *
-import time
-from tkinter.messagebox import *
+
+import tkinter
+from tkinter import messagebox
 import random
-from tkinter import Tk, Label, Button
-
-class OpeningScreen:
-    def __init__(self, master):
-        self.master = master
-        master.title("Opening")
-
-        self.label = Label(master, text="Welcome to Minesweeper!")
-        self.label.pack()
-
-        self.greet_button = Button(master, text="Help", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = Button(master, text="Close", command=root.destroy)
-        self.close_button.pack()
-
-    def greet(self):
-        print("Wanna Learn more about minesweeper, well you came to the right place")
-        print ("")
-        print ("The goal of minesweeper is to click as many boxes without detonating any bombs")
-        print ("")
-        print ("If you do this you win! Otherwise if you press a bomb it explodes and you lose")
-        print ("")
-        print ("Now have fun!")
-        print ("")
-        print (" Start by pressing close on the opening Screen")
+import time
+import math
 
 
-root = Tk()
-my_gui = OpeningScreen(root)
-root.mainloop()
+class Minesweeper():
+    def __init__(self, root, retry_diffic):
+        self.master = root
+        # set up frame
+        self.frame = tkinter.Frame(self.master)
+        self.info_frame = tkinter.Frame(self.master)  # show the number of mine, clicked, time, retry btn
+        self.init_frame = tkinter.Frame(self.master)  # choose difficulty
+        self.init_frame.pack(padx=10, pady=10)
 
-class Game:
+        # initialization
+        self.start_time = time.time()
+        self.running_time = tkinter.IntVar(value=0)
 
-    def __init__(self, master):
+        self.first_try = 1  # 1 : Preventing First Click from being Mine
+        self.max=0
+        self.flag = 0
+        self.mines = tkinter.IntVar(value=0)  # the number of mine
+        self.clicked_num = tkinter.IntVar(value=0)  # the number of clicked
+        self.Diffic_Var = tkinter.StringVar(value='0')
+        self.prob = 0
 
-        self.flags = 60
-        self.createButtons(master)
+        # import images
+        self.watch = tkinter.PhotoImage(file="graphics/timer.png")
+        self.tile_plain = tkinter.PhotoImage(file="graphics/tile_plain.gif")
+        self.tile_clicked = tkinter.PhotoImage(file="graphics/tile_clicked.gif")
+        self.tile_mine = tkinter.PhotoImage(file="graphics/tile_mine.gif")
+        self.tile_flag = tkinter.PhotoImage(file="graphics/tile_flag.gif")
+        self.tile_wrong = tkinter.PhotoImage(file="graphics/tile_wrong.gif")
+        self.tile_no = []
+        for x in range(1, 9):
+            self.tile_no.append(tkinter.PhotoImage(file="graphics/tile_" + str(x) + ".gif"))
 
-        self.bottomFrame = Frame(root)
-        self.bottomFrame.grid(row=11, columnspan=10)
+        # Game Start
+        self.set_menubar()
+        if retry_diffic == '0':
+            self.start_game()
+        else:
+            self.Diffic_Var.set(retry_diffic)
+            self.clear_init_frame()
+        self.grid_widgets()
 
-        self.flagRemainning = Label(self.bottomFrame, text='Flag Remaining : '+str(self.flags))
-        self.flagRemainning.grid(row=12)
+    def start_game(self):
+        self.Diffic_Var.set(0)
 
-        self.quitBtn = Button(self.bottomFrame, text='Quit', command=root.destroy)
-        self.quitBtn.grid(row=13, columnspan=2)
+        self.help = tkinter.Button(self.init_frame, text="Help")
+        self.help.grid(row=2, column=0, padx=20, pady=5)
 
-        self.total = 0
-        self.count = 0
-        for i in self.buttons:
-            if self.buttons[i][4][0] == 1:
-                self.total += 1
+        self.Start_btn = tkinter.Button(self.init_frame, padx=5, pady=5, text="Start", command=self.clear_init_frame)
+        self.Start_btn.grid(row=2, column=1, padx=10, rowspan=1)
 
-    def createButtons(self, parent):
-        self.buttons = {}
-        row = 0
-        col = 0
-        for x in range(0, 200):
-            status = random.choice(['safe', 'danger'])
-            self.buttons[x] = [
-            Button(parent, bg='#8a8a8a'),
-            status,
-            row,
-            col,
-            [0 if status == 'danger' else 1]
-            ]
+    def set_menubar(self):
+        menubar = tkinter.Menu(self.master)
+        self.master['menu'] = menubar
 
-            self.buttons[x][0].bind('<Button-1>', self.leftClick_w(x))
-            self.buttons[x][0].bind('<Button-3>', self.rightClick_w(x))
-            col += 1
-            if col == 20:
-                col = 0
-                row += 1
-            for k in self.buttons:
-                self.buttons[k][0].grid(row= self.buttons[k][2], column= self.buttons[k][3])
+        game_menu = tkinter.Menu(menubar, tearoff=0, bd=5)
+        menubar.add_cascade(label="Game", menu=game_menu)
+        game_menu.add_command(label="Exit", command=self.master.destroy)
 
-    def leftClick_w(self, x):
-        return lambda Button: self.leftClick(x)
+    def create_buttons(self):
+        # create buttons
+        self.buttons = dict({})
+        while True:
+            x_coord = 0
+            y_coord = 1
+            for x in range(0, self.max ** 2):
+                mine = 0
+                if random.uniform(0.0, 1.0) < self.prob:  # prob = 0.13 or 0.20 or 0,25
+                    mine = 1
+                    self.mines.set(self.mines.get() + 1)
+                # 0 = Button widget
+                # 1 = if a mine y/n (1/0)
+                # 2 = state (0 = unclicked, 1 = clicked, 2 = flagged)
+                # 3 = button id
+                # 4 = [x, y] coordinates in the grid
+                self.buttons[x] = [tkinter.Button(self.frame, image=self.tile_plain), mine, 0, x, [x_coord, y_coord]]
+                self.buttons[x][0].bind('<Button-1>', self.lclicked_wrapper(x))
+                self.buttons[x][0].bind('<Button-3>', self.rclicked_wrapper(x))
 
-    def rightClick_w(self, x):
-        return lambda Button: self.rightClick(x)
+                # calculate coords:
+                x_coord += 1
+                if x_coord == self.max:
+                    x_coord = 0
+                    y_coord += 1
 
-    def leftClick(self, btn):
-        check = self.buttons[btn][1]
-        if check == 'safe':
+            if self.mines.get() == 0:
+                continue
+            else:
+                break
 
-            self.buttons[btn][0].config(bg='green')
-            self.buttons[btn][0].config(state='disabled', relief=SUNKEN)
-            self.count += 1
-            self.nearbyMines(btn)
-            self.showNearby(btn)
-            win = self.checkWin()
-            if win:
+        # lay buttons in grid
+        for key in self.buttons:
+            self.buttons[key][0].grid(row=self.buttons[key][4][1], column=self.buttons[key][4][0])
+
+    def timer(self):
+        temp = math.floor(time.time()-self.start_time)
+        self.running_time.set(temp)
+        self.info_frame.after(500, self.timer)
+
+
+    def clear_init_frame(self):
+        self.init_frame.destroy() # destroy Choosing Diffic window
+
+        # set diff
+        self.prob = 0.20
+        self.max = 10
+
+        # set up new frame
+        self.frame.pack(padx=20, pady=10)
+        self.info_frame.pack()
+        self.create_buttons()
+        # set up timer
+        self.timer()
+
+    def lclicked(self, x):
+        # if found Mine in the first attempt.
+        if self.first_try == 1 and self.buttons[x][1] == 1:
+            self.buttons[x][1] = 0
+            self.make_one_mine()
+        else:
+            self.first_try=0
+
+        # if mine
+        if self.buttons[x][1] == 1:
+            self.show_all_mines_lose()
+            self.gameover()
+        # if not mine
+        else:
+            self.clicked_num.set(self.clicked_num.get()+1)
+            self.buttons[x][2] = 1
+
+            self.check_nearby(x)
+            self.buttons[x][0].unbind('<Button-1>')
+            if self.mines.get()+self.clicked_num.get()+self.flag == self.max**2:
+                self.show_all_mines_win()
                 self.victory()
 
-        if check == 'danger':
-            self.buttons[btn][0].config(bg='red')
-            self.buttons[btn][0].config(state='disabled', relief=SUNKEN)
-            self.lost()
+    def rclicked(self, x):
+        # if not flaged
+        if self.buttons[x][2] == 0:
+            self.buttons[x][0].config(image=self.tile_flag)
+            self.buttons[x][2] = 2
+            self.buttons[x][0].unbind('<Button-1>')
+            self.mines.set(self.mines.get() - 1)
+            self.flag += 1
+        # if flaged already
+        elif self.buttons[x][2] == 2:
+            self.buttons[x][2] = 0
+            self.buttons[x][0].config(image=self.tile_plain)
+            self.buttons[x][0].bind('<Button-1>', self.lclicked_wrapper(x))
+            self.mines.set(self.mines.get() + 1)
+            self.flag -= 1
 
-    def rightClick(self, btn):
-        if self.flags > 0:
-            self.buttons[btn][0].config(bg='blue')
-            self.buttons[btn][0].config(state='disabled', relief=SUNKEN)
-            self.flags -= 1
-            self.flagRemainning.config(text= 'Flag Remaining : '+str(self.flags))
-            if self.buttons[btn][1] == 'safe':
-                self.count += 1
+    def lclicked_wrapper(self, x):
+        return lambda Button: self.lclicked(x)
 
+    def rclicked_wrapper(self, x):
+        return lambda Button: self.rclicked(x)
+
+    def grid_widgets(self):
+        # show Title at the top
+        self.Main_Label = tkinter.Label(self.frame, text="Minesweeper")
+        self.Main_Label.grid(row=0, column=0, columnspan=30)
+        # lay information in grid
+        self.lb_mine = tkinter.Label(self.info_frame, text="Mines :")
+        self.lb_mine.grid(row=self.max + 2, column=0)
+        self.lb_mine_num = tkinter.Label(self.info_frame, textvariable=self.mines)
+        self.lb_mine_num.grid(row=self.max + 2, column=1)
+
+        self.image_time = tkinter.Button(self.info_frame, image=self.watch, borderwidth=0)
+        self.image_time.grid(row=self.max + 3, column=0)
+        self.lb_time = tkinter.Label(self.info_frame, textvariable=self.running_time)
+        self.lb_time.grid(row=self.max + 3, column=1)
+        self.retry = tkinter.Button(self.info_frame, text="Retry", command=self.retry_game, padx=1)
+        self.retry.grid(row=self.max + 3, column=3, padx=10, columnspan=3, sticky='we')
+        self.empty_lb = tkinter.Label(self.info_frame, text="   ")
+        self.empty_lb.grid(row=self.max + 2, column=2)
+
+    def make_one_mine(self):
+        while True:
+            temp_x = random.randrange(0, self.max)
+            if self.buttons[temp_x][1] == 1: # if mine
+                continue
+            elif self.buttons[temp_x][1] == 0: #if not mine
+                self.buttons[temp_x][1] = 1 # make this mine
+                break
+        self.first_try = 0
+
+    def show_all_mines_lose(self):
+        # show all mines
+        for x in self.buttons:
+            if self.buttons[x][1] == 1:  # if mine
+                self.buttons[x][0].config(image=self.tile_wrong)
+
+    def show_all_mines_win(self):
+        # show all mines
+        for x in self.buttons:
+            if self.buttons[x][1] == 1:  # if mine
+                self.buttons[x][0].config(image=self.tile_mine)
+
+    def check_nearby(self, x):
+        idx = x
+        count = 0
+        for i in [0, 1, 1]:
+            idx += i*self.max
+            for j in range(-1, 2):
+                key = idx - self.max + j
+
+                if key < 0 or key >= self.max**2:  # none exist space
+                    continue
+                elif idx % self.max == self.max - 1 and key % self.max == 0 :  # physically not close space
+                    continue
+                elif idx % self.max == 0 and key % self.max == self.max - 1:  # physically not close space
+                    continue
+                elif self.buttons[key][1] == 1:  # if mine
+                    count += 1
+
+        self.change_to_tile_no(x, count)
+
+    def change_to_tile_no(self, x, count):
+        if count != 0:
+            self.buttons[x][0].config(image=self.tile_no[count-1])
+
+        elif count == 0:
+            self.buttons[x][0].config(image=self.tile_clicked)
+            self.show_nearby_none_mine(x)
+
+
+    def show_nearby_none_mine(self, x):
+        idx = x
+        for i in [0, 1, 1]:
+            idx += i * self.max
+            for j in range(-1, 2):
+                key = idx - self.max + j
+
+                if key < 0 or key >= self.max ** 2:  # none exist space
+                    continue
+                elif idx % self.max == self.max - 1 and key % self.max == 0:  # physically not close space
+                    continue
+                elif idx % self.max == 0 and key % self.max == self.max - 1:  # physically not close space
+                    continue
+                elif self.buttons[key][1] == 0 and self.buttons[key][2] != 1:  # if not mine and unclicked
+                    self.lclicked(key)
+
+    def gameover(self):
+        ask_retry = tkinter.messagebox.askokcancel("You Lose.", "Try again?")
+        if ask_retry == True:
+            self.retry_game()
         else:
-            showinfo('no flags', 'you run out of flags')
-
-    def showNearby(self, btn):
-        if btn > 20 and btn < 190:
-            self.possible = [btn-21,btn+21, btn-20, btn+20,btn-19, btn+19,btn+1, btn-1]
-            for i in self.possible:
-                try:
-                    if self.buttons[i][1] == 'safe':
-                        if self.buttons[i][0]['bg'] == 'green':
-                            continue
-                        else:
-                            self.buttons[i][0].config(bg='green')
-                            self.buttons[i][0].config(state='disabled', relief=SUNKEN)
-                            self.count += 1
-                            self.buttons[i][4][0] == 0
-                            self.nearbyMines(i)
-                except KeyError:
-                    pass
-
-            if self.checkWin():
-                self.victory()
-
-    def nearbyMines(self, btn):
-        self.near = 0
-        if btn > 20 and btn < 190:
-            self.pos = [btn-21,btn+21, btn-20, btn+20,btn-19, btn+19,btn+1, btn-1]
-            for i in self.pos:
-                try:
-                    if self.buttons[i][1] == 'danger':
-                        self.near += 1
-                except KeyError:
-                    pass
-        if btn < 20:
-            self.pos2 = [btn+21,btn+20, btn+19,btn+1]
-            for i in self.pos:
-                try:
-                    if self.buttons[i][1] == 'danger':
-                        self.near += 1
-                except KeyError:
-                    pass
-        if btn > 190:
-            self.pos3 = [btn-21,btn-20, btn-19,btn-1]
-            for i in self.pos:
-                try:
-                    if self.buttons[i][1] == 'danger':
-                        self.near += 1
-                except KeyError:
-                    pass
-        self.buttons[btn][0].config(text=str(self.near), font=('Helvetica', 7))
-
-    def lost(self):
-        global root
-        for i in self.buttons:
-            if self.buttons[i][1] == 'danger':
-                self.buttons[i][0].config(bg='red')
-        time.sleep(1.5)
-        msg = 'you lose ! do you want to play again?'
-        answer = askquestion('play again',msg)
-        if answer == 'yes':
-            self.reset()
-        else:
-            self.quit()
+            self.master.destroy()
 
     def victory(self):
-        global root
-        msg = 'congratulations you won ! do you want to play again?'
-        answer = askquestion('play again',msg)
-        if answer == 'yes':
-            self.reset()
+        ask_retry = tkinter.messagebox.askokcancel("You Win.", "Congratulations!\nTry again?")
+        if ask_retry == True:
+            self.new_game()
         else:
-            self.quit()
+            self.master.destroy()
 
-    def reset(self):
-        self.flags = 50
-        self.flagRemainning.config(text= 'Flag Remaining : '+str(self.flags))
-        for i in self.buttons:
-            self.buttons[i][0].config(bg='#8a8a8a', text='')
-            self.buttons[i][0].config(state='normal', relief=RAISED)
-            self.buttons[i][1] = random.choice(['safe', 'danger'])
-        self.count = 0
-        self.total = 0
-        for i in self.buttons:
-            if self.buttons[i][4][0] == 1:
-                self.total += 1
+    def retry_game(self):
+        self.master.destroy()
+        game(self.Diffic_Var.get())
+    def new_game(self):
+        self.master.destroy()
+        game()
 
-    def checkWin(self):
-        return self.count == self.total
-
-    def quit(self):
-        global root
-        root.destroy()
-
-
-def main():
-    n = input ("Welcome to Minesweeper, press any key to continue: ") 
-    global root
-    root = Tk()
-    root.title('MiNeSwEePeR')
-    game = Game(root)
+def game(retry_diffic='0'):
+    root = tkinter.Tk()
+    root.title("Minesweeper")
+    Minesweeper(root, retry_diffic)
     root.mainloop()
 
-if __name__ == '__main__':
-    main()
+
+
+
+
 
